ecbdd4b2624e8550034f002a32f41ccd281f9629
Elmir Mujkić <116904225+eloom13@users.noreply.github.com>
2023-11-21 11:00:33 +0100

    login UI

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import Login from "./components/Login";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  //return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Login/>
+    </ChakraProvider>
+  )
 }
